832252e7f90b111d598c076f1360961e45b14f9b
tanvirshaad <tanvirshaad@gmail.com>
2021-10-13 00:09:32 +0600

    private route added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Login from './components/Login/Login';
 import Header from './components/Header/Header';
 import AuthProvider from './context/AuthProvider';
 import Shipping from './components/Shipping/Shipping';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
+import PlaceOrder from './components/PlaceOrder/PlaceOrder';
 
 function App() {
     return (
@@ -27,9 +29,12 @@ function App() {
                         <Route path="/register">
                             <Register></Register>
                         </Route>
-                        <Route path="/shipping">
+                        <PrivateRoute path="/shipping">
                             <Shipping></Shipping>
-                        </Route>
+                        </PrivateRoute>
+                        <PrivateRoute path="/placeorder">
+                            <PlaceOrder></PlaceOrder>
+                        </PrivateRoute>
                     </Switch>
                 </BrowserRouter>
             </AuthProvider>
---
+++ src/components/PlaceOrder/PlaceOrder.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PlaceOrder = () => {
+    return (
+        <div>
+            <h2>This is PlaceOrder</h2>
+        </div>
+    );
+};
+
+export default PlaceOrder;
